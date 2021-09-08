283ca863c2ed89a0fba90265ca6ef2313371144a
yhd <yhd@qq.com>
2021-09-08 15:30:25 +0800

    哦哦哦

---
+++ src/main/java/com/boot/vue1/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.boot.vue1.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Copyright (C), 2006-2021, 青软实训有限公司
+ * FileName: UserController
+ * Author:   asus
+ * Date:     2021/9/7 16:16
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+@RestController
+@RequestMapping("user")
+public class UserController {
+    private Integer id;
+    private String name;
+    private String author;
+}
